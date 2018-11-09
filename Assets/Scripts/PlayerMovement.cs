3db87f15b5d1a576930d9da1c2571476952f4fa7
Lucas Leplat <mesk.utp>
2018-11-10 00:19:36 +0100

    Basic Character Controller

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[Header("Movement")]
+	public float moveSpeed = 3f;
+	public float acceleration = 2f;
+	public float maxSqrVelocity = 5f;
+	public string hInput;
+	public string vInput;
+
+	[Header("Movement Modifications")]
+	public float speedMult = 1f;
+	public float dirMult = 1f;
+	public bool useInertia;
+
+	Rigidbody rigid;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rigid = GetComponent<Rigidbody>();
+	}
+
+	void Movement ()
+	{
+		float hInputValue = Input.GetAxis("Horizontal");
+		float vInputValue = Input.GetAxis("Vertical");
+		Vector3 movement;
+		if (useInertia)
+		{
+			movement = new Vector3(
+			hInputValue * dirMult * speedMult * acceleration,
+			rigid.velocity.y,
+			vInputValue * dirMult * speedMult * acceleration);
+
+			Vector3 deltaV = movement - rigid.velocity;
+			Vector3 accel = deltaV / Time.deltaTime;
+
+			if (accel.sqrMagnitude > moveSpeed * moveSpeed)
+				accel = accel.normalized * acceleration;
+
+			rigid.AddForce(accel, ForceMode.Acceleration);
+		}
+		else
+		{
+			movement = new Vector3(
+			hInputValue * dirMult * speedMult * moveSpeed,
+			rigid.velocity.y,
+			vInputValue * dirMult * speedMult * moveSpeed);
+
+			rigid.velocity = movement;
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			useInertia = !useInertia;
+		}
+	}
+
+	private void FixedUpdate ()
+	{
+		Movement();
+	}
+}
