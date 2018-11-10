317fc242ade1d665da418b9ea0968ee1669d4192
Cyril WIRTZ <cwirtz@ensc.fr>
2018-11-10 18:18:39 +0100

    Curse slippery now ok

--- Assets/Scripts/CurseManager.cs
+++ Assets/Scripts/CurseManager.cs
@@ -4,28 +4,44 @@ using UnityEngine;
 
 public class CurseManager : MonoBehaviour
 {
-    public enum CurseType { InvertMovement } //, MoveSpeedIncrease, MoveSpeedDecrease, Slippery, CameraEffect, None }
+    public enum CurseType { Slippery } //InvertMovement, MoveSpeedIncrease, MoveSpeedDecrease, CameraEffect, None }
 
-    [SerializeField] InvertMovement invertMovement;
+    [SerializeField] GameObject curseSlippery;
 
 	public void CursePlayer (GameObject player, CurseType curse)
 	{
 		Debug.Log("Player " + player.name + " cursed with " + curse.ToString());
-
         switch (curse)
         {
-            case CurseType.InvertMovement:
-                player.AddComponent<InvertMovement>();
+              case CurseType.Slippery:
+                var newCurse = Instantiate(curseSlippery, player.transform);
+                newCurse.tag = "Curse";
                 break;
-            default:
+                default:
                 break;
+    }
+}
+
+	public void UnCursePlayer (GameObject player, CurseType curseType)
+	{
+		Debug.Log("Player " + player.name + " cured from " + curseType.ToString());
+
+        var curses = GameObject.FindGameObjectsWithTag("Curse");
+        foreach (GameObject curse in curses)
+        {
+            switch (curseType)
+            {
+                case CurseType.Slippery:
+                    if (curse.GetComponent<CurseSlippery>())
+                    {
+                        Destroy(curse.gameObject);
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
     }
-
-	public void UnCursePlayer (GameObject player, CurseType curse)
-	{
-		Debug.Log("Player " + player.name + " cured from " + curse.ToString());
-	}
 
 	public void CurseAllPlayers (CurseType curse)
 	{
