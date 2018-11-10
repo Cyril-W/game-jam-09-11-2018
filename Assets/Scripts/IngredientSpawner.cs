64713f67aa496e69d1580d45ebdb505a1f0b135b
Lucas Leplat <mesk.utp>
2018-11-10 17:58:51 +0100

    Ingredient spawn : Semi check

--- Assets/Scripts/IngredientSpawner.cs
+++ Assets/Scripts/IngredientSpawner.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class IngredientSpawner : MonoBehaviour
 {
-	public int ingredientsByBatch;
+	public GameObject pickup;
+	public int ingredientsByBatch = 5;
 	public int currentBatch = 0;
-	public List<PlantEffect.Ingredient>[] ingredientsLists;
-
+	public PlantEffect.Ingredient[,] ingredientsLists;
+	int batchAmount = 0;
 	// Use this for initialization
 	void Start ()
 	{
-		
 	}
 
-	public void InitIngredientLists()
+	public void InitIngredientLists (int recipeSize)
 	{
-		ingredientsLists = new List<PlantEffect.Ingredient>[RecipeManager.instance.currentRecipeSize];
+		Debug.Log("Initialized lists");
+		Debug.Log("CurrentRecipeSize : " + recipeSize);
+		ingredientsLists = new PlantEffect.Ingredient[recipeSize, ingredientsByBatch];
+		batchAmount = recipeSize;
 	}
-	
+
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			if (currentBatch < batchAmount)
+			{
+				for (int j = 0; j < ingredientsByBatch; j++)
+				{
+					GameObject spawnedPickup = Instantiate(pickup, transform.position, transform.rotation);
+					spawnedPickup.GetComponentInChildren<PlantEffect>().ingredient = ingredientsLists[currentBatch, j];
+				}
+				currentBatch++;
+			}
+		}
 	}
 }
