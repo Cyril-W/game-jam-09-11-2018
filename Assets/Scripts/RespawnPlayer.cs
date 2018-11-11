1cee1866d28b5e7fdf9fae01daa294ac388632d2
Cyril WIRTZ <cwirtz@ensc.fr>
2018-11-11 14:21:12 +0100

    Player dies and respawns

---
+++ Assets/Scripts/RespawnPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RespawnPlayer : MonoBehaviour {
+
+    [SerializeField] float secondsToRespawn = 2f;
+    [SerializeField] AudioSource audioSource;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            var playerMovement = other.GetComponent<PlayerMovement>();
+            playerMovement.enabled = false;
+            audioSource.PlayOneShot(audioSource.clip);
+            StartCoroutine(RespawnPlayerAbove(playerMovement));
+        }
+    }
+
+    IEnumerator RespawnPlayerAbove(PlayerMovement playerMovement)
+    {
+        yield return new WaitForSeconds(secondsToRespawn);
+        playerMovement.transform.position = new Vector3(-10, 10, 0);
+        playerMovement.enabled = true;
+    }
+}
